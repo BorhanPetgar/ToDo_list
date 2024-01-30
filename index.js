bd0528808cf634d197f6a8aaa84e65f6a7e5f60c
BorhanPetgar <borhanpetgar40@gmail.com>
2024-01-31 01:36:42 +0330

    db added for login

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const { TodoListUser, Task } = require('./models/users');
+
 
 const port = 3001
 // express  app
@@ -32,8 +34,33 @@ app.get('/signup', (req, res) => {
 })
 
 app.post('/signup', (req, res) => {
-    // Perform signup logic here
-    
-    // Redirect to the root path
-    res.redirect('/');
-});
+    const { username, password } = req.body;
+  
+    // Create a new todo list user instance
+    const user = new TodoListUser({
+      username,
+      password
+    });
+  
+    // Save the user to the database
+    user.save()
+      .then((savedUser) => {
+        // Create a new task associated with the user
+        const task = new Task({
+          description: 'Your task description',
+          user: savedUser._id, // Assign the user's ObjectId to the task's user field
+        });
+  
+        // Save the task to the database
+        return task.save();
+      })
+      .then(() => {
+        // Redirect to the root path
+        res.redirect('/');
+      })
+      .catch((err) => {
+        console.log(err);
+        // Handle any error that occurs during saving the user or task
+        // Redirect or display an error message to the user
+      });
+  });
